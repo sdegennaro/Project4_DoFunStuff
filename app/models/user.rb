6a29ed6affc60028825b2ac37bbe2172cd3940b3
Sam DeGennaro <samdegennaro@Sams-MacBook-Pro.local>
2016-06-02 10:15:20 -0400

    adds friendships table, hopefully with self referential for friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-
+  has_many :friendships
+  has_many :friends, :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 end
